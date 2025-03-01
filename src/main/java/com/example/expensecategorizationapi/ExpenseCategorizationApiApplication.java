8bf09c310cfd1aec01014d35184a2e282fa1c121
yuyangsun333 <yuyangsun@ufl.edu>
2025-02-28 20:00:25 -0500

    Initial commit

---
+++ src/main/java/com/example/expensecategorizationapi/ExpenseCategorizationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.expensecategorizationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseCategorizationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseCategorizationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/expensecategorizationapi/ExpenseCategorizationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expensecategorizationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseCategorizationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
